3e8c57e26484b5d35ea824914ef5af29c4100f78
Kien Nguyen <kien.nguyen@asnet.com.vn>
2023-10-10 09:11:24 +0700

    fix out dir

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: "public",
-  },
   plugins: [
     react(),
     tsconfigPaths(),
