b57cfb1b4430e4ec41738f55efb72f36248b5810
Kien Nguyen <kien.nguyen@asnet.com.vn>
2023-10-11 09:25:35 +0700

    fix: image

--- src/mocks/dataList.ts
+++ src/mocks/dataList.ts
@@ -1,91 +1,91 @@
 export const dataItemDetail = {
   effects: [
     {
-      title: 'effects',
-      image: 'assets/images/products/color_filter.png',
-      content: 'Calming, Creative, Happy, Relaxing, Sleepy, Uplifting'
+      title: "effects",
+      image: "../assets/images/products/color_filter.png",
+      content: "Calming, Creative, Happy, Relaxing, Sleepy, Uplifting",
     },
     {
-      title: 'relieve',
-      image: '../assets/images/products/brain.png',
+      title: "relieve",
+      image: "../assets/images/products/brain.png",
       content:
-        'Anxiety, Arthritis, Chronic Pain, Depression, Fatigue, Inflammation, Insomnia, Irregular Bowel Movements, Migraines, Mood Swings'
+        "Anxiety, Arthritis, Chronic Pain, Depression, Fatigue, Inflammation, Insomnia, Irregular Bowel Movements, Migraines, Mood Swings",
     },
     {
-      title: 'aromas',
-      image: '../assets/images/products/carbon_smoke.svg',
-      content: 'Chemical, Citrus, Earthy, Pungent, Sour'
-    }
-  ]
+      title: "aromas",
+      image: "../assets/images/products/carbon_smoke.svg",
+      content: "Chemical, Citrus, Earthy, Pungent, Sour",
+    },
+  ],
 };
 
 export const DATA_LIST = [
   {
     isSoldOut: 86,
-    title: '',
+    title: "",
     totalReviews: 135,
-    discount: '$200.00',
-    price: '$102.00',
-    unit: 'unit 1',
+    discount: "$200.00",
+    price: "$102.00",
+    unit: "unit 1",
     volume: [],
-    image: 'https://tinyurl.com/2s34y377',
-    sale: 'Indica 70%',
+    image: "https://tinyurl.com/2s34y377",
+    sale: "Indica 70%",
     description:
-      'Jungle Diamonds is a slightly indica dominant hybrid strain (60% indica/40% sativa) created through crossing the infamous Slurricane X Gorilla Glue #4 strains.',
-    category: 'CONCENTRATES',
-    name: 'Mix And Match Shatter/Budder 28g (4 X 7G)',
-    rate: '46/5',
-    id: '1'
+      "Jungle Diamonds is a slightly indica dominant hybrid strain (60% indica/40% sativa) created through crossing the infamous Slurricane X Gorilla Glue #4 strains.",
+    category: "CONCENTRATES",
+    name: "Mix And Match Shatter/Budder 28g (4 X 7G)",
+    rate: "46/5",
+    id: "1",
   },
   {
     isSoldOut: 86,
-    title: '',
+    title: "",
     totalReviews: 135,
-    discount: '$200.00',
-    price: '$102.00',
-    unit: 'unit 1',
+    discount: "$200.00",
+    price: "$102.00",
+    unit: "unit 1",
     volume: [],
-    image: 'https://tinyurl.com/2s34y377',
-    sale: 'Indica 70%',
+    image: "https://tinyurl.com/2s34y377",
+    sale: "Indica 70%",
     description:
-      'Jungle Diamonds is a slightly indica dominant hybrid strain (60% indica/40% sativa) created through crossing the infamous Slurricane X Gorilla Glue #4 strains.',
-    category: 'CONCENTRATES',
-    name: 'Mix And Match Shatter/Budder 28g (4 X 7G)',
-    rate: '46/5',
-    id: '1'
+      "Jungle Diamonds is a slightly indica dominant hybrid strain (60% indica/40% sativa) created through crossing the infamous Slurricane X Gorilla Glue #4 strains.",
+    category: "CONCENTRATES",
+    name: "Mix And Match Shatter/Budder 28g (4 X 7G)",
+    rate: "46/5",
+    id: "1",
   },
   {
     isSoldOut: 86,
-    title: '',
+    title: "",
     totalReviews: 135,
-    discount: '$200.00',
-    price: '$102.00',
-    unit: 'unit 1',
+    discount: "$200.00",
+    price: "$102.00",
+    unit: "unit 1",
     volume: [],
-    image: 'https://tinyurl.com/2s34y377',
-    sale: 'Indica 70%',
+    image: "https://tinyurl.com/2s34y377",
+    sale: "Indica 70%",
     description:
-      'Jungle Diamonds is a slightly indica dominant hybrid strain (60% indica/40% sativa) created through crossing the infamous Slurricane X Gorilla Glue #4 strains.',
-    category: 'CONCENTRATES',
-    name: 'Mix And Match Shatter/Budder 28g (4 X 7G)',
-    rate: '46/5',
-    id: '1'
+      "Jungle Diamonds is a slightly indica dominant hybrid strain (60% indica/40% sativa) created through crossing the infamous Slurricane X Gorilla Glue #4 strains.",
+    category: "CONCENTRATES",
+    name: "Mix And Match Shatter/Budder 28g (4 X 7G)",
+    rate: "46/5",
+    id: "1",
   },
   {
     isSoldOut: 86,
-    title: '',
+    title: "",
     totalReviews: 135,
-    discount: '$200.00',
-    price: '$102.00',
-    unit: 'unit 1',
+    discount: "$200.00",
+    price: "$102.00",
+    unit: "unit 1",
     volume: [],
-    image: 'https://tinyurl.com/2s34y377',
-    sale: 'Indica 70%',
+    image: "https://tinyurl.com/2s34y377",
+    sale: "Indica 70%",
     description:
-      'Jungle Diamonds is a slightly indica dominant hybrid strain (60% indica/40% sativa) created through crossing the infamous Slurricane X Gorilla Glue #4 strains.',
-    category: 'CONCENTRATES',
-    name: 'Mix And Match Shatter/Budder 28g (4 X 7G)',
-    rate: '46/5',
-    id: '1'
-  }
+      "Jungle Diamonds is a slightly indica dominant hybrid strain (60% indica/40% sativa) created through crossing the infamous Slurricane X Gorilla Glue #4 strains.",
+    category: "CONCENTRATES",
+    name: "Mix And Match Shatter/Budder 28g (4 X 7G)",
+    rate: "46/5",
+    id: "1",
+  },
 ];
