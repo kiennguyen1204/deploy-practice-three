bfba3915b71c0eb1de1ee17d3f58438959046236
Kien Nguyen <kien.nguyen@asnet.com.vn>
2023-10-24 09:09:48 +0700

    fix: img url

--- src/mocks/dataList.ts
+++ src/mocks/dataList.ts
@@ -1,142 +1,142 @@
 export const dataItemDetail = {
   effects: [
     {
-      title: 'effects',
-      image: 'assets/images/products/color_filter.png',
-      content: 'Calming, Creative, Happy, Relaxing, Sleepy, Uplifting'
+      title: "effects",
+      image: "/src/assets/images/products/color_filter.png",
+      content: "Calming, Creative, Happy, Relaxing, Sleepy, Uplifting",
     },
     {
-      title: 'relieve',
-      image: 'assets/images/products/brain.png',
+      title: "relieve",
+      image: "/src/assets/images/products/brain.png",
       content:
-        'Anxiety, Arthritis, Chronic Pain, Depression, Fatigue, Inflammation, Insomnia, Irregular Bowel Movements, Migraines, Mood Swings'
+        "Anxiety, Arthritis, Chronic Pain, Depression, Fatigue, Inflammation, Insomnia, Irregular Bowel Movements, Migraines, Mood Swings",
     },
     {
-      title: 'aromas',
-      image: 'assets/images/products/carbon_smoke.svg',
-      content: 'Chemical, Citrus, Earthy, Pungent, Sour'
-    }
-  ]
+      title: "aromas",
+      image: "/src/assets/images/products/carbon_smoke.svg",
+      content: "Chemical, Citrus, Earthy, Pungent, Sour",
+    },
+  ],
 };
 
 export const DATA_LIST = [
   {
     isSoldOut: 86,
-    title: '',
+    title: "",
     totalReviews: 135,
-    discount: '$200.00',
-    price: '$102.00',
-    unit: 'unit 1',
+    discount: "$200.00",
+    price: "$102.00",
+    unit: "unit 1",
     volume: [],
-    image: 'https://tinyurl.com/2s34y377',
-    sale: 'Indica 70%',
+    image: "https://tinyurl.com/2s34y377",
+    sale: "Indica 70%",
     description:
-      'Jungle Diamonds is a slightly indica dominant hybrid strain (60% indica/40% sativa) created through crossing the infamous Slurricane X Gorilla Glue #4 strains.',
-    category: 'CONCENTRATES',
-    name: 'Mix And Match Shatter/Budder 28g (4 X 7G)',
-    rate: '46/5',
-    id: '1'
+      "Jungle Diamonds is a slightly indica dominant hybrid strain (60% indica/40% sativa) created through crossing the infamous Slurricane X Gorilla Glue #4 strains.",
+    category: "CONCENTRATES",
+    name: "Mix And Match Shatter/Budder 28g (4 X 7G)",
+    rate: "46/5",
+    id: "1",
   },
   {
     isSoldOut: 86,
-    title: '',
+    title: "",
     totalReviews: 135,
-    discount: '$200.00',
-    price: '$102.00',
-    unit: 'unit 1',
+    discount: "$200.00",
+    price: "$102.00",
+    unit: "unit 1",
     volume: [],
-    image: 'https://tinyurl.com/2s34y377',
-    sale: 'Indica 70%',
+    image: "https://tinyurl.com/2s34y377",
+    sale: "Indica 70%",
     description:
-      'Jungle Diamonds is a slightly indica dominant hybrid strain (60% indica/40% sativa) created through crossing the infamous Slurricane X Gorilla Glue #4 strains.',
-    category: 'CONCENTRATES',
-    name: 'Mix And Match Shatter/Budder 28g (4 X 7G)',
-    rate: '46/5',
-    id: '1'
+      "Jungle Diamonds is a slightly indica dominant hybrid strain (60% indica/40% sativa) created through crossing the infamous Slurricane X Gorilla Glue #4 strains.",
+    category: "CONCENTRATES",
+    name: "Mix And Match Shatter/Budder 28g (4 X 7G)",
+    rate: "46/5",
+    id: "1",
   },
   {
     isSoldOut: 86,
-    title: '',
+    title: "",
     totalReviews: 135,
-    discount: '$200.00',
-    price: '$102.00',
-    unit: 'unit 1',
+    discount: "$200.00",
+    price: "$102.00",
+    unit: "unit 1",
     volume: [],
-    image: 'https://tinyurl.com/2s34y377',
-    sale: 'Indica 70%',
+    image: "https://tinyurl.com/2s34y377",
+    sale: "Indica 70%",
     description:
-      'Jungle Diamonds is a slightly indica dominant hybrid strain (60% indica/40% sativa) created through crossing the infamous Slurricane X Gorilla Glue #4 strains.',
-    category: 'CONCENTRATES',
-    name: 'Mix And Match Shatter/Budder 28g (4 X 7G)',
-    rate: '46/5',
-    id: '1'
+      "Jungle Diamonds is a slightly indica dominant hybrid strain (60% indica/40% sativa) created through crossing the infamous Slurricane X Gorilla Glue #4 strains.",
+    category: "CONCENTRATES",
+    name: "Mix And Match Shatter/Budder 28g (4 X 7G)",
+    rate: "46/5",
+    id: "1",
   },
   {
     isSoldOut: 86,
-    title: '',
+    title: "",
     totalReviews: 135,
-    discount: '$200.00',
-    price: '$102.00',
-    unit: 'unit 1',
+    discount: "$200.00",
+    price: "$102.00",
+    unit: "unit 1",
     volume: [],
-    image: 'https://tinyurl.com/2s34y377',
-    sale: 'Indica 70%',
+    image: "https://tinyurl.com/2s34y377",
+    sale: "Indica 70%",
     description:
-      'Jungle Diamonds is a slightly indica dominant hybrid strain (60% indica/40% sativa) created through crossing the infamous Slurricane X Gorilla Glue #4 strains.',
-    category: 'CONCENTRATES',
-    name: 'Mix And Match Shatter/Budder 28g (4 X 7G)',
-    rate: '46/5',
-    id: '1'
-  }
+      "Jungle Diamonds is a slightly indica dominant hybrid strain (60% indica/40% sativa) created through crossing the infamous Slurricane X Gorilla Glue #4 strains.",
+    category: "CONCENTRATES",
+    name: "Mix And Match Shatter/Budder 28g (4 X 7G)",
+    rate: "46/5",
+    id: "1",
+  },
 ];
 
 export const LIST_PRODUCT_CATEGORY_FILTER = [
   {
-    name: 'Sales',
-    value: 12
+    name: "Sales",
+    value: 12,
   },
   {
-    name: 'Cannabis',
-    value: 430
+    name: "Cannabis",
+    value: 430,
   },
   {
-    name: 'Pre-Rolls',
-    value: 40
+    name: "Pre-Rolls",
+    value: 40,
   },
   {
-    name: 'CBD Oil',
-    value: 12
+    name: "CBD Oil",
+    value: 12,
   },
   {
-    name: 'Magic Mushrooms',
-    value: 34
+    name: "Magic Mushrooms",
+    value: 34,
   },
   {
-    name: 'Extracts',
-    value: 26
+    name: "Extracts",
+    value: 26,
   },
   {
-    name: 'Edibles',
-    value: 32
+    name: "Edibles",
+    value: 32,
   },
   {
-    name: 'Vape Pens',
-    value: 12
+    name: "Vape Pens",
+    value: 12,
   },
   {
-    name: 'Accessories',
-    value: 10
+    name: "Accessories",
+    value: 10,
   },
   {
-    name: 'Bath & Body',
-    value: 8
+    name: "Bath & Body",
+    value: 8,
   },
   {
-    name: 'Bundles',
-    value: 24
+    name: "Bundles",
+    value: 24,
   },
   {
-    name: 'Wholesale',
-    value: 28
-  }
+    name: "Wholesale",
+    value: 28,
+  },
 ];
