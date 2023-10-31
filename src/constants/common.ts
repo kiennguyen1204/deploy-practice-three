f49fda82a6ac861f02c3d0b8ca2b14b8c5726065
Kien Nguyen <kien.nguyen@asnet.com.vn>
2023-10-31 14:33:01 +0700

    update link image

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,5 +1,5 @@
 export const MAX_STAR = 5;
 
-export const SOLD_OUT = 'Product is sold out';
+export const SOLD_OUT = "Product is sold out";
 
-export const IMG_NOT_FOUND = '/src/assets/images/default_image.svg';
+export const IMG_NOT_FOUND = "assets/images/default_image.svg";
